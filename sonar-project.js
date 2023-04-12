fe0de38ef4146e439ea7e9e9dc083147836340f6
Daystarayo <99775049+Daystarayo@users.noreply.github.com>
2023-04-12 00:45:14 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.88.77:9000/',
+  serverUrl: 'http://172.31.21.228:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'c343699d7de8a326f1307294a857268f54a6af83',
+	    'sonar.login': '60afae6bd4258326958449fe8c7b3fea62f0d1cc',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
